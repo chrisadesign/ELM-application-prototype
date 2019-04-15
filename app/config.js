d5e8b0f74fe2cfbc8da62c028ed391aa0dec4605
Chris Armstrong <mail@chrisarmstrong.io>
2019-04-15 13:40:06 +0100

    Added the latest service storyboard and a placeholder index changelog.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Environment land management',
 
   // Default port that prototype runs on
   port: '3000',
